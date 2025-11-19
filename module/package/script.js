e0caa32030bc121a6f2aa702d4e6ec352edc74ad
Akbar <akbarfirdaus009@gmail.com>
2025-11-19 21:41:15 +0700

    update Tabel Kemitraan

--- module/package/script.js
+++ module/package/script.js
@@ -29,9 +29,14 @@ window.rowTemplate = function (item, index, perPage = 10) {
     <span class="font-medium sm:hidden">Pelanggan</span>  
     ${item.nama}
     </td>
+
+    <td class="px-6 py-4 text-sm text-gray-700 border-b sm:border-0 flex justify-between sm:table-cell">
+    <span class="font-medium sm:hidden">Kemitraan</span>  
+    ${item.business_category}
+    </td>
   
     <td class="px-6 py-4 text-sm text-gray-700 border-b sm:border-0 flex justify-between sm:table-cell">
-      <span class="font-medium sm:hidden">Pelanggan</span>
+      <span class="font-medium sm:hidden">Tipe</span>
       ${item.sales_type}
     </td>
   
